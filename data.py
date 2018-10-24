22287a017ad3ad9ff5dae762a1d782fea74526d6
root <root@localhost.localdomain>
2018-10-24 18:32:38 +0530

    front-end and routing done

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article one',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident incidunt eos totam ut doloremque similique, mollitia perferendis facilis adipisci officia cupiditate quidem vel quasi sequi aliquam corporis alias dolore ipsum.',
+			'author': 'Tim Saint',
+			'create_date': '24-10-2018'
+		},
+		{
+			'id': 2,
+			'title': 'Article two',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident incidunt eos totam ut doloremque similique, mollitia perferendis facilis adipisci officia cupiditate quidem vel quasi sequi aliquam corporis alias dolore ipsum.',
+			'author': 'Sim Saint',
+			'create_date': '24-10-2018'
+		},
+		{
+			'id': 3,
+			'title': 'Article three',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident incidunt eos totam ut doloremque similique, mollitia perferendis facilis adipisci officia cupiditate quidem vel quasi sequi aliquam corporis alias dolore ipsum.',
+			'author': 'Fim Saint',
+			'create_date': '24-10-2018'
+		}
+	]
+
+	return articles
---
+++ hello.py
@@ -0,0 +1,8 @@
+# Testing code for app
+
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "hello world"
